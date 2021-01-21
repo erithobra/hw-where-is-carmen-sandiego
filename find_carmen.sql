e5381c338b3c5477e7384665d2e760f7eef1d172
ericb2020 <ebrauer83@gmail.com>
2021-01-21 15:01:44 -0600

    Clue #4

--- find_carmen.sql
+++ find_carmen.sql
@@ -39,12 +39,17 @@ SELECT name FROM country WHERE code LIKE 'SMR';
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
-
+SELECT * FROM city WHERE countrycode LIKE 'SMR';
+  id  |    name    | countrycode |     district      | population
+------+------------+-------------+-------------------+------------
+ 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+ 3171 | San Marino | SMR         | San Marino        |       2294
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+SELECT * FROM city WHERE name LIKE 'SER%';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
