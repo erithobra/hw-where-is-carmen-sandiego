d43b90aefd065950edae1760ae1a25bfff037d72
ericb2020 <ebrauer83@gmail.com>
2021-01-21 15:16:29 -0600

    Clue #6

--- find_carmen.sql
+++ find_carmen.sql
@@ -67,8 +67,14 @@ Brazil
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+SELECT * FROM country WHERE name LIKE 'Brazil';
+gives result of 211 for capital
 
-
+SELECT * FROM city WHERE id = 211;
+ id  |    name    | countrycode |     district     | population
+-----+------------+-------------+------------------+------------
+ 211 | Brasï¿½lia | BRA         | Distrito Federal |    1969868
+ 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
  -- landing dock. Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
